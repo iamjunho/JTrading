0306542d60e6670a09192eaa88f4c918dc009c24
junho <iamjunho.gmail.com>
2015-05-12 21:04:40 +0900

    Querying single stock title

--- xingAPI_test.py
+++ xingAPI_test.py
@@ -2,6 +2,7 @@ __author__ = 'junho'
 
 #-*-coding: utf-8 -*-
 
+import sys
 import win32com.client
 import pythoncom
 
@@ -20,6 +21,15 @@ class XASessionEvents:
     def OnDisconnect(self):
         print("OnDisconnect method is called")
 
+class XAQueryEvents:
+    queryState = 0
+    def OnReceiveData(self, szTrCode):
+        print("ReceiveData")
+        XAQueryEvents.queryState = 1
+    def OnReceiveMessage(self, systemError, mesageCode, message):
+        print("ReceiveMessage")
+
+
 if __name__ == "__main__":
     server_addr = "demo.ebestsec.co.kr" # 모의투자의 주소는 demo.ebestsec.co.kr
                                         # 실제 거래할 때는 "hts.ebestsec.co.kr"
@@ -27,12 +37,39 @@ if __name__ == "__main__":
     server_type = 0
     user_id = "iamjunho"
     user_pass = "boribori"
-    user_certificate_pass = "abcde"
+    user_certificate_pass = "NotAvailable"
 
+    #--------------------------------------------------------------------------
+    # Login Session
+    #--------------------------------------------------------------------------
     inXASession = win32com.client.DispatchWithEvents("XA_Session.XASession", XASessionEvents)
-
     inXASession.ConnectServer(server_addr, server_port)
     inXASession.Login(user_id, user_pass, user_certificate_pass, server_type, 0)
 
     while XASessionEvents.logInState == 0:
         pythoncom.PumpWaitingMessages()
+
+    # Call the GetAccountListCount() method
+    nCount = inXASession.GetAccountListCount()
+    print("The number of account: ", nCount)
+
+    for i in range(nCount):
+        print("Account: %d - %s" % (i, inXASession.GetAccountList(i)))
+
+    #--------------------------------------------------------------------------
+    # Get single data
+    #--------------------------------------------------------------------------
+    inXAQuery = win32com.client.DispatchWithEvents("XA_DataSet.XAQuery", XAQueryEvents)
+    inXAQuery.LoadFromResFile("C:\\eBEST\\xingAPI\\Res\\t1102.res")
+    inXAQuery.SetFieldData('t1102InBlock', 'shcode', 0, '000150')
+    inXAQuery.Request(0)
+
+    while XAQueryEvents.queryState == 0:
+        pythoncom.PumpWaitingMessages()
+
+    # Get FieldData
+    name = inXAQuery.GetFieldData('t1102OutBlock', 'hname', 0)
+    price = inXAQuery.GetFieldData('t1102OutBlock', 'price', 0)
+    print("name: ", name)
+    print("price: ", price)
+    XAQueryEvents.queryState = 0
